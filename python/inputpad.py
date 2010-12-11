2857cdcc5e7f2189f7942872d956cecc91e967ba
foolegg <sd.foolegg@gmail.com>
2010-12-12 03:16:39 +0800

    fix rotate problem

--- python/inputpad.py
+++ python/inputpad.py
@@ -8,13 +8,31 @@ QtCore.Slot = QtCore.pyqtSlot
 import sys
 import time
 
+#import rotate
 from widget import NumPadKey, TextEditKey
 from backend import Backend
+
+class Rotater( QtGui.QWidget ) :
+    def __init__( self, parent = None ) :
+        QtGui.QWidget.__init__( self, parent, QtCore.Qt.Dialog | QtCore.Qt.FramelessWindowHint )
+        self.setAttribute( QtCore.Qt.WA_Maemo5PortraitOrientation, False )
+        self.setFixedHeight( 1 )
+    def resizeEvent( self, event ) :
+        #print "resize"
+        #print event.oldSize().width(), event.oldSize().height()
+        #print event.size().width(), event.size().height()
+        if event.size().width() >= 800 :
+            self.hide()
+    def closeEvent( self, event ) :
+        self.hide()
+        event.ignore()
+    #def showEvent( self, event ) :
+        #pass
 
 class InputPad( QtGui.QWidget ) :
     request_commit = QtCore.Signal( str )
     KEY_MAP = [ \
-            [ "0", [ 4, 1, 1, 1 ], 0.9, ] \
+            [ "0", [ 4, 1, 1, 1 ], 0.95, ] \
             , \
             [ "1", [ 1, 0, 1, 1 ], 1.0, ] \
             , \
@@ -34,9 +52,9 @@ class InputPad( QtGui.QWidget ) :
             , \
             [ "9", [ 3, 2, 1, 1 ], 1.0, ] \
             , \
-            [ "navigate", [ 4, 0, 1, 1 ], 0.9, ] \
+            [ "navigate", [ 4, 0, 1, 1 ], 0.95, ] \
             , \
-            [ "mode", [ 4, 2, 1, 1 ], 0.9, ] \
+            [ "mode", [ 4, 2, 1, 1 ], 0.95, ] \
             , \
             [ "backspace", [ 0, 0, 1, 3 ], 0.85, ] \
             , \
@@ -44,7 +62,7 @@ class InputPad( QtGui.QWidget ) :
     KEYCODE_NAVIGATE = 10
     KEYCODE_MODE = 11
     KEYCODE_BACKSPACE = 12
-    KEY_HEIGHT = 105
+    KEY_HEIGHT = 110
     LAYOUT_SPACING = 0
     MODE_NORMAL = 0
     MODE_SELECT = 1
@@ -63,14 +81,17 @@ class InputPad( QtGui.QWidget ) :
             ]
 
     FONT_NORMAL = QtGui.QFont()
-    FONT_UNDERLINR = QtGui.QFont()
-    FONT_UNDERLINR.setUnderline( True )
+    FONT_UNDERLINE = QtGui.QFont()
+    FONT_UNDERLINE.setUnderline( True )
     def __init__( self, daemon_flag = False, parent = None ) :
         if daemon_flag :
             QtGui.QWidget.__init__( self, parent, QtCore.Qt.Dialog | QtCore.Qt.FramelessWindowHint )
         else :
             QtGui.QWidget.__init__( self, parent )
+
         self.setAttribute( QtCore.Qt.WA_Maemo5PortraitOrientation, True )
+
+        self.rotater = Rotater()
 
         self.layout = QtGui.QVBoxLayout()
         self.layout.setSpacing( self.LAYOUT_SPACING )
@@ -79,13 +100,11 @@ class InputPad( QtGui.QWidget ) :
         self.textedit = TextEditKey( self.KEYCODE_BACKSPACE, self )
         self.textedit.clicked.connect( self.slot_key_click )
         self.textedit.longpressed.connect( self.slot_key_longpress )
-        #self.textedit.setReadOnly( True )
-        #self.textedit.grabKeyboard()
         self.layout.addWidget( self.textedit )
         
         self.keypad_layout = QtGui.QGridLayout()
         self.keypad_layout.setSpacing( self.LAYOUT_SPACING )
-        self.keypad_layout.setContentsMargins( 0, 0, 0, 0 )
+        self.keypad_layout.setContentsMargins( 0, 0, 0, 80 )
         self.layout.addLayout( self.keypad_layout )
 
         self.key_list = []
@@ -107,14 +126,16 @@ class InputPad( QtGui.QWidget ) :
         self.mode = self.MODE_NORMAL
         self.punc_index = 0
 
-        if daemon_flag :
-            self.hide()
         self.daemon_flag = daemon_flag
     def callback_show( self, string ) :
         self.textedit.setText( string )
         self.textedit.moveCursor( QtGui.QTextCursor.End )
         self.show()
-    def update( self ) :
+    def resizeEvent( self, event ) :
+        #print self.width(), self.height(), self.isVisible()
+        if self.height() < 700 :
+            self.resize( 480, 700 )
+    def context_update( self ) :
         update_stamp = []
         for i in range( len( self.KEY_MAP ) ) :
             update_stamp.append( False )
@@ -158,7 +179,7 @@ class InputPad( QtGui.QWidget ) :
             pass
         elif mode == self.MODE_SELECT :
             for i in range( 1, 7 ) :
-                self.key_list[i].setFont( self. FONT_UNDERLINR )
+                self.key_list[i].setFont( self. FONT_UNDERLINE )
         elif mode == self.MODE_PUNC :
             self.punc_index = 0
         elif mode == self.MODE_NAVIGATE :
@@ -170,14 +191,14 @@ class InputPad( QtGui.QWidget ) :
             if code >= 2 and code <= 9 :
                 self.backend.append( str( code ) )
                 self.backend.gen_cand_list()
-                self.update()
+                self.context_update()
                 #for node in self.backend.cand_list :
                     #print node[0], node[1]
             elif code == self.KEYCODE_BACKSPACE :
                 if len( self.backend.code() ) > 0 :
                     c = self.backend.pop()
                     self.backend.gen_cand_list()
-                    self.update()
+                    self.context_update()
                     if len( self.backend.code() ) <= 0 :
                         #self.key_list[code].pause_auto_repeat()
                         #self.key_list[code].disable()
@@ -189,16 +210,18 @@ class InputPad( QtGui.QWidget ) :
             elif code == 1 :
                 if len( self.backend.code() ) > 0 :
                     self.set_mode( self.MODE_SELECT )
-                    self.update()
+                    self.context_update()
                 else :
                     self.set_mode( self.MODE_PUNC )
-                    self.update()
+                    self.context_update()
             elif code == self.KEYCODE_NAVIGATE :
                 if len( self.backend.code() ) > 0 :
                     pass
                 else :
                     self.set_mode( self.MODE_NAVIGATE )
-                    self.update()
+                    self.context_update()
+            elif code == self.KEYCODE_MODE :
+                self.setAttribute( QtCore.Qt.WA_Maemo5PortraitOrientation, False )
         elif self.mode == self.MODE_SELECT :
             if code >= 1 and code <= 6 :
                 self.backend.select( code - 1 )
@@ -208,38 +231,38 @@ class InputPad( QtGui.QWidget ) :
                     self.backend.commit()
                     self.textedit.textCursor().insertText( text, self.textedit.normal_format )
                     self.set_mode( self.MODE_NORMAL )
-                self.update()
+                self.context_update()
             elif code == self.KEYCODE_BACKSPACE :
                 c = self.backend.deselect()
                 self.backend.gen_cand_list()
                 if c == "" :
                     self.set_mode( self.MODE_NORMAL )
-                self.update()
+                self.context_update()
             elif code == 7 :
                 self.backend.page_prev()
                 self.backend.gen_cand_list()
-                self.update()
+                self.context_update()
             elif code == 9 :
                 self.backend.page_next()
                 self.backend.gen_cand_list()
-                self.update()
+                self.context_update()
         elif self.mode == self.MODE_PUNC :
             if code >= 2 and code <= 9 :
                 index = code - 2
                 punc_list = self.PUNC_MAP[self.punc_index]
                 self.textedit.textCursor().insertText( punc_list[index].decode( "utf-8" ), self.textedit.normal_format )
                 self.set_mode( self.MODE_NORMAL )
-                self.update()
+                self.context_update()
             elif code == self.KEYCODE_BACKSPACE :
                 self.set_mode( self.MODE_NORMAL )
-                self.update()
+                self.context_update()
             elif code == 1 :
                 self.punc_index = self.punc_index + 1
                 if self.punc_index < len( self.PUNC_MAP ) :
                     pass
                 else :
                     self.punc_index = 0
-                self.update()
+                self.context_update()
         elif self.mode == self.MODE_NAVIGATE :
             if code == self.KEYCODE_NAVIGATE :
                 self.set_mode( self.MODE_NORMAL )
@@ -264,8 +287,9 @@ class InputPad( QtGui.QWidget ) :
         pass
     def closeEvent( self, event ) :
         if self.daemon_flag :
-            event.ignore()
             self.hide()
+            self.rotater.show()
+            event.ignore()
             self.textedit.set_preedit( "" )
             self.set_mode( self.MODE_NORMAL )
             text = self.textedit.toPlainText()
