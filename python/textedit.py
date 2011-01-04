ab50a6496927e7e1a3cb4076c639bbcffab6ba97
foolegg <sd.foolegg@gmail.com>
2011-01-04 23:43:19 +0800

    modify navigate

--- python/textedit.py
+++ python/textedit.py
@@ -10,8 +10,6 @@ class TextEdit( QtGui.QTextEdit ) :
     longpressed = QtCore.Signal( int )
     clicked = QtCore.Signal( int )
     DEFAULT_LONGPRESS_INTERVAL = 350
-    DEFAULT_MOVE_CONTROL_INTERVAL = 350
-    DEFAULT_MOVE_INTERVAL = 35
     DEFAULT_AUTO_REPEAT_INTERVAL = 65
     def __init__( self, keycode, parent = None ) :
         QtGui.QTextEdit.__init__( self, parent )
@@ -24,13 +22,9 @@ class TextEdit( QtGui.QTextEdit ) :
         self.preedit_end_pos = -1
         self.preedit = ""
         self.start_pos = None
+        self.start_cursor = None
         self.external_flag = False
         self.move_flag = False
-        self.move_control_timer = QtCore.QTimer()
-        self.move_control_flag = True
-        self.move_control_timer.timeout.connect( self.move_control_timeout )
-        self.move_timer = QtCore.QTimer()
-        self.move_timer.timeout.connect( self.__move )
 
         self.auto_repeat_timer = QtCore.QTimer()
         self.auto_repeat_flag = True
@@ -91,73 +85,51 @@ class TextEdit( QtGui.QTextEdit ) :
         self.mousePressEvent( event )
     def mousePressEvent( self, event ) :
         self.auto_repeat_timer.stop()
-        self.move_control_flag = True
-        self.move_control_timer.stop()
-        self.move_timer.stop()
         self.start_pos = event.pos()
+        self.start_cursor = self.textCursor()
         self.move_flag = False
         self.timer.start( self.longpress_interval )
-    @QtCore.Slot()
-    def __move( self ) :
-        self.move_timer.stop
-        self.move_timer.start( self.DEFAULT_MOVE_INTERVAL )
-        pos = self.mapFromGlobal( QtGui.QCursor.pos() )
-        rect = self.cursorRect()
-        top, bottom = rect.top(), rect.bottom()
-        height = self.height()
-        old_cursor = self.textCursor()
-        start_cursor = self.textCursor()
-        start_cursor.movePosition( QtGui.QTextCursor.StartOfLine )
-        end_cursor = self.textCursor()
-        end_cursor.movePosition( QtGui.QTextCursor.EndOfLine )
-        y = pos.y()
-        if y < -0 :
-            y = top - 1
-        elif y >= -0 and y <= height + 50 :
-            y = top
-        elif y > height + 30 :
-            y = bottom + 1
-        pos.setY( y )
-        x = pos.x()
-        pos.setX( x - 10 )
-        new_cursor = self.cursorForPosition( pos )
-        self.setTextCursor( new_cursor )
-        rect = self.cursorRect()
-        if new_cursor < start_cursor or new_cursor >= end_cursor :
-            if not self.move_control_flag :
-                self.setTextCursor( old_cursor )
-            else :
-                self.move_control_flag = False
-                self.move_control_timer.start( self.DEFAULT_MOVE_CONTROL_INTERVAL )
-
     def mouseMoveEvent( self, event ) :
-        if self.move_flag :
-            self.__move()
+        if len( self.preedit ) > 0 or self.external_flag :
+            pass
         else :
             pos = event.pos()
-            x = self.start_pos.x() - event.x()
-            y = self.start_pos.y() - event.y()
-            d = x * x + y * y
-            #print d
-            if d > 1024 :
-                self.move_flag = True
+            if self.move_flag :
+                width = self.width()
+                d = pos.x() - self.start_pos.x()
+                d = int( 10 * d / width )
+                new_pos = self.start_cursor.position() + d
+                cursor = self.textCursor()
+                if new_pos < 0 :
+                    new_pos = 0
+                else :
+                    cursor.movePosition( QtGui.QTextCursor.End )
+                    end_pos = cursor.position() 
+                    if new_pos > end_pos :
+                        new_pos = end_pos
+                cursor.setPosition( new_pos )
+                self.setTextCursor( cursor )
+            else :
+                x = self.start_pos.x() - event.x()
+                y = self.start_pos.y() - event.y()
+                d = x * x + y * y
+                #print d
+                if d > 1024 :
+                    self.move_flag = True
     def mouseReleaseEvent( self, event ) :
         self.auto_repeat_timer.stop()
-        self.move_control_flag = True
-        self.move_control_timer.stop()
-        self.move_timer.stop()
         if self.timer.isActive() :
             self.timer.stop()
             x = event.pos().x()
-            if self.move_flag :
-                pass
-            elif len( self.preedit ) > 0 or self.external_flag :
+            if len( self.preedit ) > 0 or self.external_flag :
                 self.clicked.emit( self.keycode )
             else :
                 width = self.width()
                 if x - self.start_pos.x() > width / 10 :
+                    self.setTextCursor( self.start_cursor )
                     self.__move_end()
                 elif self.start_pos.x() - x > width / 10 :
+                    self.setTextCursor( self.start_cursor )
                     self.__move_start()
                 else :
                     self.__delete()
@@ -173,9 +145,6 @@ class TextEdit( QtGui.QTextEdit ) :
         else :
             self.__delete()
         self.ensureCursorVisible()
-    @QtCore.Slot()
-    def move_control_timeout( self ) :
-        self.move_control_flag = True
     @QtCore.Slot()
     def timeout( self ) :
         self.timer.stop()
